e1e98cc8c529a2319a69a94c515408320c3aaeb7
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2020-12-04 17:20:14 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+	extend ActiveHash::Associations::ActiveRecordExtensions
+	belongs_to :state
+	belongs_to :category
+	belongs_to :shipping_burden
+	belongs_to :shipper_prefecture
+	belongs_to :shipping_days
 	belongs_to :user
 	has_one_attached :image
 end
