327ad79ca839be6ee2c24ae8050c045a58b344e1
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2020-12-04 14:31:14 +0900

    マイグレーションの実行

---
+++ db/migrate/20201204052317_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string     :name,                   null: false
+      t.text       :information,            null: false
+      t.integer    :price,                  null: false
+      t.references :user,                   null: false, foreign_key:true
+      t.integer    :state_id,               null: false
+      t.integer    :category_id,            null: false
+      t.integer    :shipping_burden_id,     null: false
+      t.integer    :shipper_prefecture_id,  null: false
+      t.integer    :shipping_days_id,       null: false
+      t.timestamps
+    end
+  end
+end
