0f34777306bdd8ff761227c1bd22aa9d4fdfb4e8
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2020-12-09 11:51:42 +0900

    destroyアクションに出品者のみ削除ができるように条件分岐を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.user == current_user
+      @item.destroy
+    end
     redirect_to root_path
   end
 
