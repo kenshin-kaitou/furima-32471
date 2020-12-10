ea1f53813232c2faf3d1a01fa949da72125404d2
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2020-12-10 19:16:38 +0900

    カード情報のトークンを受け取る処理を追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,7 +5,18 @@ class PurchasesController < ApplicationController
     @shipping = Shipping.new
   end
 
+  def create
+    @purchase = Purchase.new(purchases_params)
+    if @purchase.valid?
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
   private
-
+  def purchases_params
+    params.require(:purchase).merge(token: params[:token], user_id: current_user_id, item_id: params[:item_id])
+  end
   
 end
