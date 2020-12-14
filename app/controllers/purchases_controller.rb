316e6e56cf5bb091ff774425909d95236c1267c8
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2020-12-14 11:35:44 +0900

    ログイン状態でないユーザーが直接アクセスできないよう修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
     @item = Item.find(params[:item_id])
     if @item.user == current_user
