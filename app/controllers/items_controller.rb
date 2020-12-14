08595f9073d704cd8ba7e82b8ac526720b5d4b8f
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2020-12-14 17:17:43 +0900

    購入済みの商品にsold outを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :get_item, only: [:show, :edit, :update, :destroy]
   def index
-    @items = Item.includes(:user).order('created_at DESC')
+    @items = Item.includes(:user,:purchase).order('created_at DESC')
   end
 
   def new
